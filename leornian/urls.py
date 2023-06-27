8d0dbc2026625d50a7d012a3b897d5cd1e5eb4f7
DJ Ramones <50655786+djramones@users.noreply.github.com>
2023-06-27 18:05:31 +0800

    Add provisional home page

--- leornian/urls.py
+++ leornian/urls.py
@@ -2,10 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 admin.site.site_header = admin.site.site_title = "Leornian admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
