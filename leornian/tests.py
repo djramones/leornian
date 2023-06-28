d50f58325749cb3bf2bc08f0f3dbdfabdb1a6b59
DJ Ramones <50655786+djramones@users.noreply.github.com>
2023-06-28 20:47:44 +0800

    Expand basic get tests

--- leornian/tests.py
+++ leornian/tests.py
@@ -2,6 +2,19 @@ from django.test import TestCase
 
 
 class BasicTests(TestCase):
-    def test_home_page(self):
+    def test_basic_site_views_get(self):
         response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get("/accounts/me/")
+        self.assertRedirects(response, "/accounts/login/?next=/accounts/me/")
+
+        # Admin views
+        response = self.client.get("/admin/")
+        self.assertRedirects(response, "/admin/login/?next=/admin/")
+
+        # Auth views
+        response = self.client.get("/accounts/login/")
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get("/accounts/logout/")
         self.assertEqual(response.status_code, 200)
