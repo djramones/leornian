5459ef7ded033a3aa339287957cbb071e3a1fd93
DJ Ramones <50655786+djramones@users.noreply.github.com>
2023-10-06 00:02:17 +0800

    Add simple core package tests

--- leornian/tests.py
+++ leornian/tests.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.core.handlers.asgi import ASGIHandler
+from django.core.handlers.wsgi import WSGIHandler
 from django.test import TestCase
+
+UserModel = get_user_model()
 
 
 class BasicTests(TestCase):
-    def test_basic_site_views_get(self):
+    def test_basic_requests_unauthenticated(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
@@ -18,3 +23,21 @@ class BasicTests(TestCase):
         self.assertEqual(response.status_code, 200)
         response = self.client.get("/accounts/logout/")
         self.assertRedirects(response, "/")
+
+    def test_basic_requests_authenticated(self):
+        UserModel.objects.create_user("mary", "mary@example.com", "1234")
+        self.client.login(username="mary", password="1234")
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class XSGISmokeTests(TestCase):
+    def test_asgi_import(self):
+        from leornian import asgi
+
+        self.assertIs(type(asgi.application), ASGIHandler)
+
+    def test_wsgi_import(self):
+        from leornian import wsgi
+
+        self.assertIs(type(wsgi.application), WSGIHandler)
